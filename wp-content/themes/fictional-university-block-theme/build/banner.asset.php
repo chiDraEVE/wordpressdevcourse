8e0fa71a89ddf9711e2c7f76dad20b3ff948e3b0
chidraeve <chidraeve@clubitsolutions.de>
2022-12-13 20:55:07 +0100

    181. Quick details: Banner Block

--- wp-content/themes/fictional-university-block-theme/build/banner.asset.php
+++ wp-content/themes/fictional-university-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'b0854b1258feb2eb6289');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '659b0bf83badf05c0765');
