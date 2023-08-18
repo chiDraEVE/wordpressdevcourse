e2167454199b295ff1e2182d9c48e8048007df27
chidraeve <chidraeve@clubitsolutions.de>
2023-08-18 14:13:42 +0200

    added identical theme into repository. I guess me or brad did give it a different name at some point

---
+++ wp-content/themes/fictional-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ffa1e67d01e14e2e6686');
---
+++ wp-content/themes/fictional-block-theme/build/slide.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '094d96ccc6e8ff28f70e');
